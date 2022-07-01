6f573dd1b6600e3dc3cd56f27066d46d0ec5c619
rostamn739 <rostamn739Wgmail.com>
2022-07-01 16:54:26 +0300

    wip

--- lalrpop/src/lr1/codegen/base.rs
+++ lalrpop/src/lr1/codegen/base.rs
@@ -220,13 +220,11 @@ impl<'codegen, 'grammar, W: Write, C> CodeGenerator<'codegen, 'grammar, W, C> {
             }
             type_parameters = vec![
                 format!(
-                    "{}TOKEN: {}ToTriple<{}>",
-                    self.prefix, self.prefix, user_type_parameters,
+                    "{}TOKENS: lalrpop_util::state_machine::ParserFeedback<D, E>",
+                    self.prefix,
                 ),
-                format!(
-                    "{}TOKENS: IntoIterator<Item={}TOKEN>",
-                    self.prefix, self.prefix
-                ),
+                "D: lalrpop_util::state_machine::ParserDefinition".to_owned(),
+                "E".to_owned(),
             ];
             parameters = vec![format!("{}tokens0: {}TOKENS", self.prefix, self.prefix)];
             where_clauses = vec![];
@@ -314,7 +312,7 @@ impl<'codegen, 'grammar, W: Write, C> CodeGenerator<'codegen, 'grammar, W, C> {
             let clone_call = if self.repeatable { ".clone()" } else { "" };
             rust!(
                 self.out,
-                "let {}tokens = {}tokens0{}.into_iter();",
+                "let {}tokens = {}tokens0{};",
                 self.prefix,
                 self.prefix,
                 clone_call
